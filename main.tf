7c4c626bad6299842fc5ddd86ec6ed8d3767273b
Liam Jones <hello@ljones.id.au>
2021-11-22 12:07:06 +0000

    Fix helm provider

--- main.tf
+++ main.tf
@@ -71,7 +71,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  kubernetes {
   host                   = digitalocean_kubernetes_cluster.doks.kube_config[0].host
   token                  = digitalocean_kubernetes_cluster.doks.kube_config[0].token
   cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.doks.kube_config[0].cluster_ca_certificate)
+  }
 }
